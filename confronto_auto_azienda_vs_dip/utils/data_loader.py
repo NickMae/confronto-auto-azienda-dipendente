c5cd2781fc1206608e3214f869adbedd20346edf
NickMae <n.maestroni95@gmail.com>
2025-04-27 21:13:10 +0200

    Update data_loader.py

--- confronto_auto_azienda_vs_dip/utils/data_loader.py
+++ confronto_auto_azienda_vs_dip/utils/data_loader.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import os
 
 def load_aci_data():
-    fringe_data = pd.read_csv("data/fringe_benefit_2025.csv")
+    base_path = os.path.dirname(__file__)  # <-- directory corrente, cioè utils/
+    data_path = os.path.join(base_path, "..", "data", "fringe_benefit_2025.csv")
+    data_path = os.path.normpath(data_path)  # pulisce il percorso per Windows/Linux
+    fringe_data = pd.read_csv(data_path)
     fringe_data = fringe_data.dropna()
     return fringe_data
